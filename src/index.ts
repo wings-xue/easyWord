7bc1ecfddfbdeaee7c2d36fb64aa3bae636d0a8d
薛闻斯 <xuews@hsmap.com>
2019-03-11 19:15:53 +0800

    express + typeorm init

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,59 @@
-import { ConnectionOptions } from "typeorm"
+import { ConnectionOptions, createConnection, Connection } from "typeorm"
 import { root } from "./paths"
 import { EnglishWord } from "./entity/wordStack"
 
 import * as express from "express";
 import * as bodyParser from "body-parser";
+import { ENAMETOOLONG } from "constants";
 
-
+const PORT = 3000;
 const options: ConnectionOptions = {
   type: "sqlite",
   database: `${root}/data/line.sqlite`,
   entities: [EnglishWord],
-  logging: true
-}
+  logging: true,
+  synchronize: true,
 
-class App {
-
-  public app: express.Application;
-
-  constructor() {
-    this.app = express();
-    this.say();
-  }
-
-  say() {
-    this.app.get('/', function (req, res) {
-      res.send('Hello World!')
-    })
-  }
-
-  runServer(port: number) {
-    this.app.listen(port, () => {
-      console.log('Express server listening on port ' + PORT)
-    })
-  }
 }
 
 
-const PORT = 3000;
-let app = new App();
-app.runServer(PORT);
+// create typeorm connection
+createConnection(options).then(connection => {
+  const Repository = connection.getRepository(EnglishWord);
+
+  // create and setup express app
+  const app = express();
+  app.use(bodyParser.json());
+
+  // register routes
+  app.get('/', function (req, res) {
+    res.send('Hello World!');
+  });
 
 
+  app.get("/update", async function (req, res) {
+    return Repository.find();
+  });
+
+  app.get("/find", async function (req, res) {
+    return Repository.findOne();
+  });
+
+  app.get("/insert", async (req, res) => {
+    let englishWord = new EnglishWord();
+    englishWord.word = "ceshi";
+    await Repository.save(englishWord);
+    res.send("succeed")
+  });
+
+
+  // start express server
+  app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+  });
+
+
+});
 
 
 
