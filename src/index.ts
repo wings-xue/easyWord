ff015aef9d0835692ccee3f7d50f480739d7df77
xuewensi <lenglengzailuguan@gmail.com>
2019-03-14 00:06:51 +0800

    单词添加与更新接口（未测试）

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,7 @@ const options: ConnectionOptions = {
 }
 
 
+
 // create typeorm connection
 createConnection(options).then(connection => {
   const Repository = connection.getRepository(EnglishWord);
@@ -31,19 +32,38 @@ createConnection(options).then(connection => {
   });
 
 
-  app.get("/update", async function (req, res) {
-    return Repository.find();
+  app.get("/insert", async function (req, res) {
+    let englishWord = new EnglishWord();
+    englishWord.word = req.query.word;
+    englishWord.description = req.query.description;
+    englishWord.sentence = req.query.sentence;
+    englishWord.translate = req.query.translate;
+    englishWord.nextDate = new Date();
+    englishWord.views = 1;
+    englishWord.remember = false; 
+    return Repository.save(englishWord);
   });
 
   app.get("/find", async function (req, res) {
     return Repository.findOne();
   });
 
-  app.get("/insert", async (req, res) => {
-    let englishWord = new EnglishWord();
-    englishWord.word = "ceshi";
-    await Repository.save(englishWord);
-    res.send("succeed")
+
+
+  app.get("/update", async (req, res) => {
+    let word = req.query.word
+    let isRemeber = req.query.remember
+    let review = req.query.review
+  
+   
+    let englisWord = await Repository.findOne({word: word});
+    englisWord.nextDate = new Date()
+    if (review == "ok") {
+      englisWord.remember = true;
+    }
+    await Repository.save(englisWord);
+    console.log("save " + word + "succeed");
+    res.send("000")
   });
 
 
